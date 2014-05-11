3990f50eb5c1b881312093b4ae54722c9da5db56
Scott Willeke <scott@willeke.com>
2014-05-11 14:17:53 -0700

    Install more dependencies on a clean machine. Propper convention for grunt-cli install.

--- build.sh
+++ build.sh
@@ -27,6 +27,10 @@ echo "Installing npm packages declared in package.json..."
 npm install
 echo "Installing npm packages complete."
 
+echo "Checking for bower..."
+command -v bower > /dev/null || { echo "Bower not installed. installing..."; npm install -g bower; }
+echo "Checking / installing bower complete."
+
 # bower: Ensure bower depdnencies are installed.
 #  Installs client-side JavaScript dependencies.
 #  Actual dependencies are declared in bower.json
@@ -35,6 +39,16 @@ bower install
 echo "Installing bower packages complete."
 
 # tsd: NOTE: Installing TypeScript definition files not really needed. Generally those are to be committed to the source repo.
+echo "Checking for tsd..."
+command -v tsd > /dev/null || { echo "tsd not installed. installing..."; npm install -g tsd; }
+echo "Checking / installing tsd complete."
+tsd update
+
+echo "Checking for grunt..."
+command -v grunt > /dev/null || { echo "Grunt not installed. installing..."; npm install -g grunt-cli; }
+echo "Checking / installing grunt complete."
+
+
 
 # grunt: Grunt is the build tool that runs the build script Gruntfile.js and the with no arguments the default task registered therein.
 grunt
